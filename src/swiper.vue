d1c41c8d84b9c5be7f3ab7595fa1069b18b54043
duanyuwen <duanyuwen@meituan.com>
2018-08-23 14:04:57 +0800

    修复在微信小程序中使用没有window对象而报错问题

--- src/swiper.vue
+++ src/swiper.vue
@@ -14,7 +14,7 @@
 <script>
   // require sources
   import _Swiper from 'swiper/dist/js/swiper.js'
-  const Swiper = window.Swiper || _Swiper
+  const Swiper = (window && window.Swiper) || _Swiper
 
   // pollfill
   if (typeof Object.assign != 'function') {
